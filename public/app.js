50617777d6e842f61b818d2727149541a749389f
Dandun Adi <dandun.adi@gmail.com>
2021-06-01 06:03:33 +0700

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("Mario", "Create blog", 400);
+var invTwo = new Invoice("Luigi", "Create portfolio", 200);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 var form = document.querySelector(".new-item-form");
 var type = document.querySelector("#type");
 var toFrom = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(client: string, details: string, amount: number) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("Mario", "Create blog", 400);
+const invTwo = new Invoice("Luigi", "Create portfolio", 200);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 const type = document.querySelector("#type") as HTMLSelectElement;
