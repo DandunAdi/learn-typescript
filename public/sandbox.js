cf894067ed9ce08b0bc289f5e657b4d64aa8c8fd
Dandun Adi <dandun.adi@gmail.com>
2021-06-01 03:12:23 +0700

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Hello there!");
+var greet = function () {
+    console.log("Hello");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log("C is: " + c);
+};
+add(1, 2);
+var substract = function (a, b) {
+    return a - b;
+};
+console.log(substract(10, 15));
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,14 @@
-console.log("Hello there!");
+const greet = () => {
+  console.log("Hello");
+};
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log("C is: " + c);
+};
+add(1, 2);
+
+const substract = (a: number, b: number): number => {
+  return a - b;
+};
+console.log(substract(10, 15));
